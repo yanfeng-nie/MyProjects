c1c7787a266b5ffa170d88f5f20fb398c74d5f6a
Yanfeng Nie <61353821+yanfeng-nie@users.noreply.github.com>
2025-07-27 22:54:55 +0800

    Create quick_sort.cpp

---
+++ cpps/algorithm/quick_sort/quick_sort.cpp
@@ -0,0 +1,19 @@
+/***********************************************************
+  > File Name: quick_sort.cpp
+  > Author: Nie Yanfeng
+  > Mail: e83242650@163.com
+  > Created Time: Sun 27 Jul 2025 10:52:52 PM CST
+  > Last Modified: Sun 27 Jul 2025 10:52:56 PM CST
+***********************************************************/
+
+#include<cstdio>
+#include<iostream>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+int main(int argc, char* argv[]){
+
+    return 0;
+}
+
